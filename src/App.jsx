ca77f6604e3393e77107dd0e308543e73ee521a8
daniel <danemokhor@gmail.com>
2025-08-27 19:18:45 +0100

    Admin Page

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>🛍️ JOY Clothing Store</h1>
+      <h1>🛍️ JOY Clothing Store Admin Page</h1>
 
       
       <div className="filters">
